a40cb7f3c4e8c6f09d4412d6f1e4cae9f3b91628
VivoBook <m.subandhi@students.amikom.ac.id>
2025-10-21 21:52:47 +0700

    initial commit

--- app/src/main/java/com/example/a3128_login/Login.kt
+++ app/src/main/java/com/example/a3128_login/Login.kt
@@ -1,6 +1,9 @@
 package com.example.a3128_login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,26 @@ import androidx.core.view.WindowInsetsCompat
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.login)
+
+        val et_Username: EditText = findViewById (R.id.et_Username)
+        val et_Pass: EditText = findViewById (R.id.et_Pass)
+        val btn_login: Button = findViewById (R.id.btn_login)
+        val btn_daftar: Button = findViewById(R.id.btn_daftar)
+
+        btn_login.setOnClickListener {
+            val username = et_Username.text.toString()
+
+            val pindah : Intent= Intent(this, Dashboard:: class.java)
+
+            pindah.putExtra("nama", username)
+
+            startActivity(pindah)
+        }
+
+        btn_daftar.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
         }
     }
 }
